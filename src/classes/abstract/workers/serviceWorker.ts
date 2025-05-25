aa5610c7858e72da126fbebea2f9bdd01095fc1a
Franco Canalejo <fcanalejo2000@outlook.com>
2025-05-25 13:05:00 +1000

    enhance event handling in WorkerService and update type definitions

--- src/classes/abstract/workers/serviceWorker.ts
+++ src/classes/abstract/workers/serviceWorker.ts
@@ -1,19 +1,19 @@
 import { Worker } from "worker_threads";
 import AppError from "@Class/error.js";
 import path from "path";
-import { ServiceWorkerBaseEventHandler, ServiceWorkerBasePayload } from "@Type/express/workers.js";
+import { ServiceWorkerBaseEventHandler, ServiceWorkerBaseEvents, ServiceWorkerBasePayload } from "@Type/express/workers.js";
 import { rootPath } from "get-root-path";
 import Base from "@/classes/abstract/common/base.js";
 
 export default class WorkerService extends Base {
-	protected static createRunner<Payload = unknown, Operation = string, Events = string>(
+	protected static createRunner<Payload = unknown, Operation = string, Events extends string | number | symbol = string>(
 		workerPath: string,
 	) {
 		return {
-			run: <ReturnData = Payload>(
-				payload: ServiceWorkerBasePayload<Payload, Operation, Events>,
-				events?: ServiceWorkerBaseEventHandler<ReturnData, Operation, Events>,
-			): Promise<ReturnData> => {
+			run: <ReturnData = Payload, EventData = Payload | ReturnData>(
+            payload: ServiceWorkerBasePayload<Payload, Operation, Events>,
+            events?: Partial<Record<ServiceWorkerBaseEvents<Events>, (data: ServiceWorkerBasePayload<EventData, Operation, Events>) => void>>,
+        ): Promise<ReturnData> => {
 				return new Promise((resolve, reject) => {
 					this.Logger.d(payload, "Running worker with payload");
 					const worker = new Worker(path.join(rootPath, workerPath), {
@@ -33,7 +33,7 @@ export default class WorkerService extends Base {
 								  >);
 
 							if (events && Object.keys(events).length > 0 && event && event in events) {
-								return events[event](response as any);
+								return events[event]?.(response as any);
 							}
 
 							return response.status === "success"
--- src/types/express/workers.ts
+++ src/types/express/workers.ts
@@ -1,17 +1,17 @@
 import AppError from "@Class/error.js";
 
-export type ServiceWorkerBaseStatus = 'success' | 'error';
-export type ServiceWorkerBaseEvents<Events = string> = 'start' | 'progress' | 'complete' | 'error' | 'cancelled' | 'status' | 'update' | 'message' | Events;
 
-export type ServiceWorkerBaseEventHandlerFunction<EventData = string> = (payload: EventData) => void
- 
-export type ServiceWorkerBaseEventHandler<Data = string, Operations = string, Events = string> = {
-    [event in ServiceWorkerBaseEvents & Events]: ServiceWorkerBaseEventHandlerFunction<ServiceWorkerBasePayload<Data, Operations>>;
-};
-export interface ServiceWorkerBasePayload<Data = unknown, Operations = string, Events = string> {
+export type ServiceWorkerBaseStatus = 'success' | 'error';
+export type ServiceWorkerBaseEvents<CustomEvents = never> = 'start' | 'progress' | 'complete' | 'error' | 'cancelled' | 'status' | 'update' | 'message' | CustomEvents;
+
+export interface ServiceWorkerBasePayload<Data = unknown, Operations = string, Events = never> {
     operation: Operations;
     payload: Data;
     status?: ServiceWorkerBaseStatus;
     error?: AppError;
     event?: ServiceWorkerBaseEvents<Events>;
 }
+
+export type ServiceWorkerBaseEventHandler<Data = unknown, Operations = string, Events = never, EventData = Data> = {
+    [event in ServiceWorkerBaseEvents<Events extends string ? Events : never>]?: (payload: ServiceWorkerBasePayload<EventData, Operations, Events>) => void;
+};
