0ea827492eb978e953f1ffd3d117b2b800501a2f
Franco Canalejo <fcanalejo2000@outlook.com>
2025-05-25 13:19:05 +1000

    refactor event handler types in WorkerService and update payload handling

--- src/classes/abstract/workers/serviceWorker.ts
+++ src/classes/abstract/workers/serviceWorker.ts
@@ -1,19 +1,25 @@
 import { Worker } from "worker_threads";
 import AppError from "@Class/error.js";
 import path from "path";
-import { ServiceWorkerBaseEventHandler, ServiceWorkerBaseEvents, ServiceWorkerBasePayload } from "@Type/express/workers.js";
+import {
+	ServiceWorkerBaseEventHandler,
+	ServiceWorkerBaseEvents,
+	ServiceWorkerBasePayload,
+} from "@Type/express/workers.js";
 import { rootPath } from "get-root-path";
 import Base from "@/classes/abstract/common/base.js";
 
 export default class WorkerService extends Base {
-	protected static createRunner<Payload = unknown, Operation = string, Events extends string | number | symbol = string>(
-		workerPath: string,
-	) {
+	protected static createRunner<
+		Payload = unknown,
+		Operation = string,
+		Events extends string | number | symbol = string,
+	>(workerPath: string) {
 		return {
 			run: <ReturnData = Payload, EventData = Payload | ReturnData>(
-            payload: ServiceWorkerBasePayload<Payload, Operation, Events>,
-            events?: Partial<Record<ServiceWorkerBaseEvents<Events>, (data: ServiceWorkerBasePayload<EventData, Operation, Events>) => void>>,
-        ): Promise<ReturnData> => {
+				payload: ServiceWorkerBasePayload<Payload, Operation, Events>,
+				events?: Partial<Record<ServiceWorkerBaseEvents<Events>, (data: string) => void>>,
+			): Promise<ReturnData> => {
 				return new Promise((resolve, reject) => {
 					this.Logger.d(payload, "Running worker with payload");
 					const worker = new Worker(path.join(rootPath, workerPath), {
@@ -26,14 +32,10 @@ export default class WorkerService extends Base {
 							this.Logger.d(response, "Worker response");
 							const event = !response.event
 								? null
-								: (response.event as keyof ServiceWorkerBaseEventHandler<
-										Payload,
-										Operation,
-										Events
-								  >);
+								: (response.event as keyof ServiceWorkerBaseEventHandler<Events>);
 
 							if (events && Object.keys(events).length > 0 && event && event in events) {
-								return events[event]?.(response as any);
+								return events[event]?.(JSON.stringify(response));
 							}
 
 							return response.status === "success"
--- src/types/express/workers.ts
+++ src/types/express/workers.ts
@@ -12,6 +12,6 @@ export interface ServiceWorkerBasePayload<Data = unknown, Operations = string, E
     event?: ServiceWorkerBaseEvents<Events>;
 }
 
-export type ServiceWorkerBaseEventHandler<Data = unknown, Operations = string, Events = never, EventData = Data> = {
-    [event in ServiceWorkerBaseEvents<Events extends string ? Events : never>]?: (payload: ServiceWorkerBasePayload<EventData, Operations, Events>) => void;
+export type ServiceWorkerBaseEventHandler<Events = never> = {
+    [event in ServiceWorkerBaseEvents<Events extends string ? Events : never>]?: (payload: string) => void;
 };
